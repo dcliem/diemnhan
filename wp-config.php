fb32a37f266e5eed4b10378e9d913750ba839006
dcliem <doancongliem@gmail.com>
2017-05-15 07:55:23 +0000

    Change data connect

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'diemnhan_db');
+define('DB_NAME', 'diemnhan_db2');
 
 /** MySQL database username */
 define('DB_USER', 'diemnhan_us');
