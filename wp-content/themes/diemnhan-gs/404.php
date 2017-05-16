027500336c58d8f618f0087c57b53adac334dc33
dcliem <doancongliem@gmail.com>
2017-05-16 08:42:29 +0700

    Remove diemnhan-gs from project

--- wp-content/themes/diemnhan-gs/404.php
+++
@@ -1,9 +0,0 @@
-<?php
-//* Loop Content
-remove_action( 'genesis_loop', 'genesis_do_loop' );
-add_action( 'genesis_loop', 'page_content_main' );
-function page_content_main() { ?>
-    Nội dung truy cập không tồn tại
-    <?php
-}
-genesis();
--- wp-content/themes/diemnhan-gs/front-page.php
+++
@@ -1,7 +0,0 @@
-<?php
-//* Loop Content
-remove_action( 'genesis_loop', 'genesis_do_loop' );
-add_action( 'genesis_loop', 'front_content_main' );
-function front_content_main() { global $NZ;
-}
-genesis();
